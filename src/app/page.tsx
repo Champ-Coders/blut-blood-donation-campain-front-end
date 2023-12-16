e2c8bdf102bd773af22469a1a68b6e01c3926734
masud90895 <masudhossainmbs129@gmail.com>
2023-12-16 11:17:07 +0600

    Add new components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Banner from "@/components/Home/Banner";
 import Blog from "@/components/Home/Blog";
+import BloodOwner from "@/components/Home/BloodOwner";
+import CallNow from "@/components/Home/CallNow";
 import Counter from "@/components/Home/Counter";
 import HelpThePeopleInNeed from "@/components/Home/HelpThePeopleInNeed";
+import News from "@/components/Home/News";
+import PopularCampaigns from "@/components/Home/PopularCampaigns";
+import Services from "@/components/Home/Services";
+import TeamMember from "@/components/Home/TeamMember";
+import Testimonials from "@/components/Home/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +17,13 @@ export default function Home() {
       <Blog />
       <HelpThePeopleInNeed />
       <Counter />
+      <Services />
+      <CallNow />
+      <PopularCampaigns />
+      <Testimonials />
+      <BloodOwner />
+      <TeamMember />
+      <News />
     </>
   );
 }
