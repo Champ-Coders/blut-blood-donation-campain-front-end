123372e426635879f2eade099c3bc8d0fac2c7ff
masud90895 <masudhossainmbs129@gmail.com>
2023-12-16 11:10:10 +0600

    Add swiper library for carousel functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,11 @@ import "./globals.css";
 import Providers from "@/lib/Providers";
 import HeaderPage from "@/components/shared/Header/Header";
 import FooterPage from "@/components/shared/Footer/Footer";
+// slick-carousel
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+// swiper
+import "swiper/css";
 
 const inter = Inter({ subsets: ["latin"] });
 
