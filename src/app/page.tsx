5c78356757cbec38d887e1267b26b089803a1290
masud90895 <masudhossainmbs129@gmail.com>
2023-12-16 11:15:53 +0600

    Add components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Banner from "@/components/Home/Banner";
+import Blog from "@/components/Home/Blog";
+import Counter from "@/components/Home/Counter";
+import HelpThePeopleInNeed from "@/components/Home/HelpThePeopleInNeed";
+
 export default function Home() {
-  return <div>Main</div>;
+  return (
+    <>
+      <Banner />
+      <Blog />
+      <HelpThePeopleInNeed />
+      <Counter />
+    </>
+  );
 }
---
+++ src/components/Home/CallNow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CallNow = () => {
+  return <div>Call Now</div>;
+};
+
+export default CallNow;
