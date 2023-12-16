cfb69ccb9a243f96365c7c017940223d9f5e8012
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-16 17:53:58 +0600

    feat: added bg img

--- src/components/Home/CallNow.tsx
+++ src/components/Home/CallNow.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-
 const CallNow = () => {
-  return <div>Call Now</div>;
+  return (
+    <div className="min-h-[calc(100vh-64px)]">
+      <div
+        style={{
+          backgroundImage: `url(https://innovativeartisan.com/demo/html/blad-ai/assets/images/call.jpg)`,
+        }}
+        className={`bg-cover h-[calc(100vh-64px)] relative py-[90px]`}
+      >
+        <div className="absolute h-full inset-0 bg-black/80"></div>
+        <div className="container mx-auto max-w-5xl bg-red-500">
+          Call To action
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default CallNow;
