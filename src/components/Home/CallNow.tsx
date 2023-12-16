81efe161a0ec2d92276c3cf006a7a75584922ed5
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-16 18:23:28 +0600

    feat:added call content

--- src/components/Home/CallNow.tsx
+++ src/components/Home/CallNow.tsx
@@ -5,11 +5,26 @@ const CallNow = () => {
         style={{
           backgroundImage: `url(https://innovativeartisan.com/demo/html/blad-ai/assets/images/call.jpg)`,
         }}
-        className={`bg-cover h-[calc(100vh-64px)] relative py-[90px]`}
+        className={`bg-cover h-auto sm:h-[calc(100vh-64px)] relative py-[90px]`}
       >
-        <div className="absolute h-full inset-0 bg-black/80"></div>
-        <div className="container mx-auto max-w-5xl bg-red-500">
-          Call To action
+        <div className="absolute h-auto inset-0 bg-black/80"></div>
+        <div className="w-full h-full font-poppins flex justify-center items-center">
+          <div className="container f mx-auto max-w-5xl">
+            <div className="py-[70px] text-white border-8 border-primary relative text-center block">
+              <p>START DONATING</p>
+              <h1 className="my-6 text-5xl">
+                <span className="font-semibold">Call Now:</span> +8801935399238
+              </h1>
+              <div className="flex justify-center items-center gap-5">
+                <p className="text-base transition duration-300 ease-in hover:text-primary">
+                  Dhaka, Bangladesh
+                </p>
+                <p className="text-base transition duration-300 ease-in hover:text-primary">
+                  Donate@gamil.com
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
