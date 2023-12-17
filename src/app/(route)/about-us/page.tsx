1bfa8f3a36820bc550415fd94a43a84ee71d3906
f4faysal <f4faysals@gmail.com>
2023-12-17 15:14:12 +0600

    Add BannerBreadcrumb component to AboutPage

--- src/app/(route)/about-us/page.tsx
+++ src/app/(route)/about-us/page.tsx
@@ -1,5 +1,20 @@
+import BannerBreadcrumb from "@/components/UI/BannerBreadcrumb";
+
 const AboutPage = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <BannerBreadcrumb
+        items={[
+          {
+            label: "About",
+          },
+        ]}
+        title="About Us"
+        image="/assets/blood-donor-bg.png"
+      />
+      about
+    </div>
+  );
 };
 
 export default AboutPage;
