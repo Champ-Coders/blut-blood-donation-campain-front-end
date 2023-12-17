547672660202dd5c99ab4ea16a680869a1bf1af4
Hasan-Rifat <developerhr000@gmail.com>
2023-12-17 16:45:37 +0600

    Update styles and add social media links to TeamMembersCard component

--- src/components/Home/TeamMember.tsx
+++ src/components/Home/TeamMember.tsx
@@ -9,7 +9,7 @@ const TeamMember = () => {
             <h5 className="text-[#EA062B] text-[16px] uppercase mb-4">
               TEAM MEMBERS
             </h5>
-            <h4 className="capitalize text-2xl lg:text-5xl font-bold text-white mb-7">
+            <h4 className="capitalize text-2xl lg:text-5xl font-bold text-[#111111] mb-7">
               Meet Volunteers
             </h4>
           </div>
--- src/components/TeamMembersCard/TeamMembersCard.tsx
+++ src/components/TeamMembersCard/TeamMembersCard.tsx
@@ -1,8 +1,40 @@
+import Image from "next/image";
 import React from "react";
 
-type TeamMembersCardProps = {};
+type TeamMembersCardProps = {
+  name: string;
+  position: string;
+  image: string;
+  socialMedia: {
+    id: string;
+    link: string;
+    icon: string;
+  }[];
+};
 
-const TeamMembersCard: React.FC<TeamMembersCardProps> = () => {
-  return <div>Have a good coding</div>;
+const TeamMembersCard: React.FC<TeamMembersCardProps> = ({
+  name,
+  position,
+  image,
+  socialMedia,
+}) => {
+  return (
+    <div className="p-[25px]">
+      <div className="relative">
+        <Image src={""} alt="" />
+        <div className="absolute left-0 top-0 w-full h-full z-10">
+          <ul className="flex gap-1 ">
+            {socialMedia.map((item) => (
+              <li key={item.id}>{item.link}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="mt-[34px]">
+        <h4 className="text-[#666666] mb-5"></h4>
+        <p></p>
+      </div>
+    </div>
+  );
 };
 export default TeamMembersCard;
