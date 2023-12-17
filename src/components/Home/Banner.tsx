4ce8839e81a80926e3b25cd0b361a46b66467bb4
MDMinhazulIslamKhan <minhazulislamkhan@gmail.com>
2023-12-17 12:29:44 +0600

    #task-12=>LandingPage=>Banner update

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -4,8 +4,8 @@ import { YoutubeFilled } from "@ant-design/icons";
 import Link from "next/link";
 const Banner = () => {
   return (
-    <div className="relative">
-      <Carousel autoplay>
+    <div>
+      <Carousel autoplay arrows dotPosition="top" className="-z-10">
         {bannerCarousel.map((item) => (
           <div key={item._id}>
             <div
@@ -15,19 +15,19 @@ const Banner = () => {
                 width: "100%",
                 backgroundSize: "cover",
               }}
-              className="sm:h-[70vh] h-96 px-20 py-10 sm:py-20 text-center sm:text-left"
+              className="h-screen sm:px-20 px-10 py-20 sm:py-40 text-center sm:text-left"
             >
               <a href={item.videoURL} target="_blank">
                 <YoutubeFilled className="text-primary text-5xl" />
               </a>
-              <h3 className="text-primary text-lg sm:text-3xl mt-2 font-bold">
+              <h3 className="text-primary text-lg sm:text-3xl mt-8 font-bold">
                 {item.quotes}
               </h3>
-              <h3 className="text-black text-2xl sm:text-6xl mt-2 sm:mt-5 font-extrabold">
+              <h3 className="text-black text-2xl sm:text-6xl max-w-xl mt-2 sm:mt-5 font-extrabold">
                 {item.quotes}
               </h3>
               <Link href={item.link}>
-                <button className="btn bg-primary mt-8 hover:bg-black text-white text-xl px-5 py-2 rounded-lg">
+                <button className="btn bg-primary mt-16 hover:bg-black text-white text-xl px-5 py-2 rounded-lg">
                   Explore Now
                 </button>
               </Link>
@@ -35,12 +35,12 @@ const Banner = () => {
           </div>
         ))}
       </Carousel>
-      <div className="flex sm:flex-row flex-col top-20 text-white cursor-pointer">
-        <div className="bg-black hover:bg-gray-600 px-10 w-full h-44 text-justify">
+      <div className="flex w-full md:w-5/6 mx-0 md:mx-auto sm:flex-row flex-col top-20 text-white cursor-pointer -mt-20 z-80">
+        <div className="bg-black hover:bg-gray-600 px-10 w-full h-44">
           <h3 className="text-2xl mt-5 sm:mt-8 mb-4 font-extrabold">
             Register Now
           </h3>
-          <p className="">
+          <p className="text-sm">
             Nor again is there anyone who loves or pursues or desires to obtain
             pain of itself, because it is pain,
           </p>
@@ -49,7 +49,7 @@ const Banner = () => {
           <h3 className="text-2xl mt-5 sm:mt-8 mb-4 font-extrabold">
             Donate Now
           </h3>
-          <p className="">
+          <p className="text-sm">
             Nor again is there anyone who loves or pursues or desires to obtain
             pain of itself, because it is pain,
           </p>
