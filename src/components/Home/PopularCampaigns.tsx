852231341eb29cf97310135e2b527dfe2a25e734
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-17 21:21:46 +0600

    feat: added populer camping title and subtitle

--- src/components/Home/PopularCampaigns.tsx
+++ src/components/Home/PopularCampaigns.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const PopularCampaigns = () => {
-  return <div>PopularCampaigns</div>;
-};
+  return (
+    <div className="bg-[#f5f5f5]">
+      <div className="text-center mb-5 sm:mb-10">
+        <h3 className="text-base sm:text-xl text-primary font-medium mb-3 uppercase">
+        DONATE NOW
+        </h3>
+        <h1 className="text-black font-[800] text-3xl sm:text-5xl">
+        Popular Campaigns
+        </h1>
+      </div>
 
-export default PopularCampaigns;
+      
+    </div>
+  )
+}
+export default PopularCampaigns
