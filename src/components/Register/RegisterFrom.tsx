c20c87c1f26e06b1f0a5b49d34823c8de3838471
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-18 14:49:09 +0600

    feat: added full name input filed

--- src/components/Register/RegisterFrom.tsx
+++ src/components/Register/RegisterFrom.tsx
@@ -1,6 +1,76 @@
-const RegisterFrom = () => {
-  return (
-    <div>RegisterFrom</div>
-  )
+"use client";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import InputField from "../InputField/InputField";
+import MultiSelect from "../MultiSelector/MultiSelector";
+import TextAreaField from "../TextAreaField/TextAreaField";
+import Button from "../Button/Button";
+
+type AppointmentFormProps = {};
+
+interface IFormSelector {
+  value: string;
+  label: string;
 }
-export default RegisterFrom
+[];
+
+const options: IFormSelector[] = [
+  {
+    value: "Donation Type",
+    label: "Donation Type",
+  },
+  {
+    value: "Free Donation",
+    label: "Free Donation",
+  },
+  {
+    value: "Health Checkup",
+    label: "Health Checkup",
+  },
+];
+
+const AppointmentForm: React.FC<AppointmentFormProps> = () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    // Handle form submission logic here
+    console.log("Form Data:", data);
+  };
+
+  return (
+    <form className="block w-full" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex justify-between items-start gap-5">
+        <p className="mt-4 -mb-1 min-w-[160px] font-semibold text-lg">
+          Full Name *
+        </p>
+
+        <div className="flex-grow">
+          <div className="flex justify-between items-center gap-6">
+            <InputField
+              label="First Name"
+              name={"first_name"}
+              type="text"
+              register={register}
+              errors={errors}
+            />
+            <InputField
+              label="Last Name"
+              name={"last_name"}
+              type="text"
+              register={register}
+              errors={errors}
+            />
+          </div>
+        </div>
+      </div>
+
+      <Button type="submit">Submit</Button>
+    </form>
+  );
+};
+export default AppointmentForm;
