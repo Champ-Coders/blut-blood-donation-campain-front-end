3a7f9a7be3e9025f95a2b40e2f60c597df702433
masud90895 <masudhossainmbs129@gmail.com>
2023-12-18 15:20:47 +0600

    Update global CSS and add compatible blood type donors table

--- src/components/BloodDonorType/CompatibleBloodTypeDonors.tsx
+++ src/components/BloodDonorType/CompatibleBloodTypeDonors.tsx
@@ -1,10 +1,100 @@
-import React from 'react';
+"use client";
+import { Table } from "antd";
+import Image from "next/image";
+import React from "react";
+
+const CompatibleBloodType = [
+  {
+    bloodType: "A+",
+    donateBloodTo: "A+, AB+",
+    receiveBloodFrom: "A+, A-, O+, O-",
+  },
+  {
+    bloodType: "A-",
+    donateBloodTo: "A+, A-, AB+, AB-",
+    receiveBloodFrom: "A-, O-",
+  },
+  {
+    bloodType: "B+",
+    donateBloodTo: "B+, AB+",
+    receiveBloodFrom: "B+, B-, O+, O-",
+  },
+  {
+    bloodType: "B-",
+    donateBloodTo: "B+, B-, AB+, AB-",
+    receiveBloodFrom: "B-, O-",
+  },
+  {
+    bloodType: "AB+",
+    donateBloodTo: "AB+",
+    receiveBloodFrom: "Everyone",
+  },
+  {
+    bloodType: "AB-",
+    donateBloodTo: "AB+, AB-",
+    receiveBloodFrom: "AB-, A-, B-, O-",
+  },
+  {
+    bloodType: "O+",
+    donateBloodTo: "A+, B+, AB+, O+",
+    receiveBloodFrom: "O+, O-",
+  },
+  {
+    bloodType: "O-",
+    donateBloodTo: "Everyone",
+    receiveBloodFrom: "O-",
+  },
+];
+
+const columns = [
+  {
+    title: "Blood Type",
+    dataIndex: "bloodType",
+    key: "bloodType",
+  },
+  {
+    title: "Donate Blood To",
+    dataIndex: "donateBloodTo",
+    key: "donateBloodTo",
+  },
+  {
+    title: "Receive Blood From",
+    dataIndex: "receiveBloodFrom",
+    key: "receiveBloodFrom",
+  },
+];
 
 const CompatibleBloodTypeDonors = () => {
   return (
-    <div>
-      <h1></h1>
-      
+    <div className="md:w-5/6 mx-0 md:mx-auto ">
+      <h1 className="text-[28px] font-semibold my-[30px] text-center text-primary font-poppins">
+        Learn About Donation
+      </h1>
+
+      {/* donation */}
+
+      <div className="flex flex-col lg:flex-row w-full gap-28 items-center justify-between">
+        {/* image */}
+        <div>
+          <Image
+            src="https://i.ibb.co/TtssLxt/donation-Fact.webp"
+            alt="logo"
+            width={600}
+            height={700}
+            className=" w-[807px] h-[330px] "
+          />
+        </div>
+
+        {/* Table */}
+        <Table
+          columns={columns}
+          dataSource={CompatibleBloodType}
+          bordered
+          title={() => "Compatible Blood Type Donors"}
+          pagination={false}
+          className="w-[600px] shadow-xl rounded-lg"
+        />
+      </div>
     </div>
   );
 };
