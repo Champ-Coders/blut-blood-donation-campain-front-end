0ecba60cbc32b0bf161852891fce9668674514b3
masud90895 <masudhossainmbs129@gmail.com>
2023-12-19 11:53:07 +0600

    Update page title in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Providers";
-import HeaderPage from "@/components/shared/Header/Header";
-import FooterPage from "@/components/shared/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blut | Blood Donation Campaign",
+  title: "Dashboard | Blood Donation Campaign",
   description: "Blut to the main page of the Blood Donation Campaign",
 };
 
