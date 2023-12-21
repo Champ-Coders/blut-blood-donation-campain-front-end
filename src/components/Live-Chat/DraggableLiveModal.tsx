4a88ffe10947201327a48a77dfed78cd7b208a1d
masud90895 <masudhossainmbs129@gmail.com>
2023-12-21 22:59:09 +0600

    Refactor Live Chat component layout and add Chat Now button

--- src/components/Live-Chat/DraggableLiveModal.tsx
+++ src/components/Live-Chat/DraggableLiveModal.tsx
@@ -12,20 +12,29 @@ type DraggableLiveModalProps = {
 const DraggableLiveModal = () => {
   return (
     <div>
-      <div className="h-[400px] w-[400px] overflow-x-hidden bg-[#f0f0f5] px-3 py-2 flex gap-2 font-inter ">
-        <Image
-          src={"https://i.ibb.co/YcjhGgs/IMG-20231111-142014-1.jpg"}
-          alt="Live Chat"
-          width={4000}
-          height={4000}
-          className="w-[20px] h-[20px] rounded-full"
-        />
+      <div className="h-[400px] w-[400px] overflow-x-hidden bg-[#f0f0f5] px-3 py-2 font-inter flex flex-col justify-between ">
+        {/* message */}
+        <div className=" flex gap-2">
+          <Image
+            src={"https://i.ibb.co/YcjhGgs/IMG-20231111-142014-1.jpg"}
+            alt="Live Chat"
+            width={4000}
+            height={4000}
+            className="w-[20px] h-[20px] rounded-full"
+          />
 
-        <div className="bg-white shadow-lg rounded-lg w-4/5  h-[30px] px-2 pt-1 mt-1 text-black">
-          Our team typically replies in a few minutes.
+          <div className="bg-white shadow-lg rounded-lg w-4/5  h-[30px] px-2 pt-1 mt-1 text-black">
+            Our team typically replies in a few minutes.
+          </div>
         </div>
+
+        {/* add Chat Now button on footer */}
+
+        <button className="bg-primary text-white rounded-lg px-2 py-1 w-full hover:bg-black">
+          Chat Now
+        </button>
       </div>
-      <footer className="flex gap-1 text-center w-full items-center justify-around py-1">
+      <footer className="flex gap-1 text-center w-full items-center justify-around py-1 border-t">
         <div className="flex gap-1 items-center">
           Power by <FaRegMessage className="text-primary font-bold" />{" "}
           <span className="font-bold">Live Chat</span>
