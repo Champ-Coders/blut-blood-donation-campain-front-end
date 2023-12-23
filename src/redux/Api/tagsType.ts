c3ac33bfa20aa1f0096a14259167e0ce3464b9cb
Hasan-Rifat <developerhr000@gmail.com>
2023-12-23 21:22:04 +0600

    Add service tag type to tagTypesList

--- src/redux/Api/tagsType.ts
+++ src/redux/Api/tagsType.ts
@@ -1,9 +1,15 @@
 export enum tagTypes {
   user = "user",
   blog = "blog",
-
+  service = "service",
   faq = "faq",
   event = "event",
 }
 
-export const tagTypesList = [tagTypes.user, tagTypes.faq, tagTypes.blog,tagTypes.event];
+export const tagTypesList = [
+  tagTypes.user,
+  tagTypes.faq,
+  tagTypes.blog,
+  tagTypes.event,
+  tagTypes.service,
+];
