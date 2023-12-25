de16fe793258fa7a133c35916bb1cf5575e0e6a8
masud90895 <masudhossainmbs129@gmail.com>
2023-12-25 23:03:48 +0600

    Add react-quill dependency and update NewsCard and CallNow components

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -40,9 +40,14 @@ const NewsCard: React.FC<NewsCardProps> = ({ news }) => {
           <h3 className="text-2xl text-[#111111] font-bold mb-[9px]">
             {news?.title}
           </h3>
-          <p className="text-[#666] text-[14px]">
-            {news?.description.slice(0, 150)}
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: news?.description.slice(0, 150),
+            }}
+            className="text-[#666] text-[14px]"
+          />
+          {/* {news?.description.slice(0, 150)}
+          </p> */}
           <Link
             className="mt-[16px] flex gap-1 items-center font-bold hover:text-[#ea062b] transition duration-300"
             href={`blog/${news?.id}`}
---
+++ src/components/ReactQuill/ReactQuill.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+type ReactQuillTextProps = {
+  value?: string;
+  setValue?: (value: string) => void;
+  label?: string;
+  required?: boolean;
+};
+
+const ReactQuillText = ({
+  value,
+  setValue,
+  label,
+  required,
+}: ReactQuillTextProps) => {
+  return (
+    <div className="">
+      {label && (
+        <div className="flex gap-1 items-center mb-1">
+          <label
+            className={`text-[13px] leading-6 font-inter text-gray-400 font-semibold capitalize`}
+          >
+            {label} {required && <span className="text-rose-500">*</span>}
+          </label>
+        </div>
+      )}
+      <ReactQuill
+        modules={{
+          toolbar: [
+            [{ header: "1" }, { header: "2" }, { font: [] }],
+            [{ size: [] }],
+            ["bold", "italic", "underline", "strike", "blockquote"],
+            [
+              { list: "ordered" },
+              { list: "bullet" },
+              { indent: "-1" },
+              { indent: "+1" },
+            ],
+            [{ color: [] }, { background: [] }],
+            ["link", "image"],
+            ["clean"],
+          ],
+        }}
+        formats={[
+          "header",
+          "font",
+          "size",
+          "bold",
+          "italic",
+          "underline",
+          "strike",
+          "blockquote",
+          "list",
+          "bullet",
+          "indent",
+          "color",
+          "background",
+          "link",
+          "image",
+        ]}
+        theme="snow"
+        value={value}
+        style={{ maxHeight: "400px" }}
+        onChange={setValue}
+        placeholder={"Write Something..."}
+        className="h-64 px-4 py-2 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent mb-[70px] "
+      />
+    </div>
+  );
+};
+
+export default ReactQuillText;
