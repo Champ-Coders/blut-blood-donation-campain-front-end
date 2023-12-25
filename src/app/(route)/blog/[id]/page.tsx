627a3bbb4b96194a05f1e575e65f24e97ae64a25
masud90895 <masudhossainmbs129@gmail.com>
2023-12-25 23:38:10 +0600

    Refactor page.tsx: Add user icon, link, and filter data

--- src/app/(route)/blog/[id]/page.tsx
+++ src/app/(route)/blog/[id]/page.tsx
@@ -7,14 +7,28 @@ import Button from "@/components/Button/Button";
 import config from "@/config/config";
 import Comments from "@/components/Comments/Comments";
 
+import userIcon from "../../../../../public/assets/icon/userIcon.png";
+import Link from "next/link";
+
 type BlogDetailProps = {
   params: { id: string };
 };
 
-async function getData(id: string) {
-  const res = await fetch(`${config.apiBaseUrl}/blog/${id}`);
+async function getAllBlog() {
+  const res = await fetch(`${config.apiBaseUrl}/blog`);
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function getData(id: string) {
+  const res = await fetch(`${config.apiBaseUrl}/blog/${id}`);
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
@@ -31,6 +45,18 @@ const BlogDetail: React.FC<BlogDetailProps> = async ({
 }) => {
   const newData = await getData(params.id);
   const data = newData?.data;
+
+  // created at
+  const createdTime: any = new Date(data?.createdAt).toDateString();
+
+  // console.log(data);
+  const allBlog = await getAllBlog();
+
+  // filter data allBlog.data !=== data._id
+  const filterData = allBlog?.data?.filter(
+    (item: any) => item._id !== data._id
+  );
+
   return (
     <main>
       <section>
@@ -49,40 +75,108 @@ const BlogDetail: React.FC<BlogDetailProps> = async ({
             <div className="grid-cols-1 md:col-span-3 lg:col-span-8">
               <div>
                 <h2 className="text-5xl text-[#111] font-bold mb-7 font-playfair">
-                  {data.title}
+                  {data?.title}
                 </h2>
 
                 <div className="h-[400px] overflow-hidden mt-5 mb-10">
                   <Image
                     width={839}
                     height={438}
-                    src={data.image}
-                    alt={data.title}
+                    src={data?.image}
+                    alt={data?.title}
                   />
                 </div>
-                <p className="text-base text-[#666] leading-7 font-roboto">
-                  {data.description}
-                </p>
+
+                {/* author */}
+
+                <div className="flex items-center mb-5">
+                  <div className="mr-5">
+                    <Image
+                      src={data?.user?.image ?? userIcon}
+                      width={50}
+                      height={50}
+                      alt={data?.author?.name}
+                      className="w-[30px] h-[30px] rounded-full"
+                    />
+                  </div>
+                  <div>
+                    <h5 className="text-[#111]  text-[14px]">
+                      {data?.user?.name}
+                    </h5>
+                    <p className="text-[#666] font-roboto text-[12px]">
+                      {createdTime}
+                    </p>
+                  </div>
+                </div>
+
+                <p
+                  className="text-base text-[#666] leading-7 font-roboto"
+                  dangerouslySetInnerHTML={{ __html: data?.description }}
+                />
+                {/* {data.description}
+                </p> */}
               </div>
             </div>
-            <div className="grid-cols-1 md:col-span-3 lg:col-span-4">
-              <div className="bg-primary p-5 text-white ">
-                <h5 className="text-3xl font-semibold mb-5 font-playfair">
-                  Blood Excellence
-                </h5>
-                <h3 className="text-4xl font-semibold mb-5 font-playfair">
-                  Expanded Blood Donate Services Here
-                </h3>
-                <p className="mb-5 font-roboto">
-                  There are many variations of passages Lorem Ipsum available,
-                  but the majority suffered of alteration in some form,
-                </p>
-                <Button
-                  type="button"
-                  className="!bg-[#111] text-white hover:bg-white hover:text-[#111] transition duration-300 "
-                >
-                  Contact Us
-                </Button>
+            <div
+              className="grid-cols-1 md:col-span-3 lg:col-span-4
+             h-screen sticky top-24"
+            >
+              <div className="hidden py-2 xl:col-span-3 lg:col-span-4 md:hidden lg:block">
+                <div className="mb-8 space-x-5 border-b-2 border-opacity-10 ">
+                  <button
+                    type="button"
+                    className="pb-5 text-xs font-bold uppercase border-b-2 border-primary "
+                  >
+                    Latest
+                  </button>
+                  <button
+                    type="button"
+                    className="pb-5 text-xs font-bold uppercase border-b-2  "
+                  >
+                    Popular
+                  </button>
+                </div>
+                <div className="flex flex-col divide-y  ">
+                  {/* Side Blog */}
+
+                  {filterData?.map((item: any, i: number) => (
+                    <Link
+                      href={`/blog/${item?._id}`}
+                      key={i}
+                      className="flex px-1 py-4"
+                    >
+                      <Image
+                        src={item?.image}
+                        alt={item?.title}
+                        width={500}
+                        height={500}
+                        className="flex-shrink-0 object-cover w-20 h-20 mr-4 "
+                      />
+
+                      <div className="flex flex-col flex-grow">
+                        <p className="font-serif hover:underline">
+                          {item?.title.slice(0, 30) + "..."}
+                        </p>
+                        {/* description */}
+
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: item?.description.slice(0, 50) + "...",
+                          }}
+                        />
+
+                        <div className="mt-auto text-xs ">
+                          {/*add 5 minutes ago  */}
+                          {
+                            <time dateTime="2020-03-16">
+                              {new Date(item?.createdAt).toDateString()}
+                            </time>
+                          }
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
