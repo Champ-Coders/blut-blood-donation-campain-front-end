47d5441457ec8e0bf5b352ed42266785b8808de1
masud90895 <masudhossainmbs129@gmail.com>
2023-12-26 21:33:28 +0600

    Remove react-chat-plugin and update LiveChatButton component

--- src/components/Live-Chat/LiveChatButton.tsx
+++ src/components/Live-Chat/LiveChatButton.tsx
@@ -1,93 +1,18 @@
 "use client";
 
 import { Badge, FloatButton, Tooltip } from "antd";
-import ChatBox, { ChatFrame } from "react-chat-plugin";
 import Image from "next/image";
 import React, { useState } from "react";
 import LiveChatImage from "../../../public//assets/live-chat-icon.png";
 import Popovers from "../Popover/Popover";
 import DraggableLiveModal from "./DraggableLiveModal";
-import { FaMinus } from "react-icons/fa";
+
 
 const LiveChatButton = () => {
   const [open, setOpen] = React.useState(false);
 
 
-  const [attr, setAttr] = useState({
-    showChatbox: false,
-    showIcon: true,
-    messages: [
-      {
-        text: "user2 has joined the conversation",
-        timestamp: 1578366389250,
-        type: "notification",
-      },
-      {
-        author: {
-          username: "user1",
-          id: 1,
-          avatarUrl: "https://image.flaticon.com/icons/svg/2446/2446032.svg",
-        },
-        text: "Hi",
-        type: "text",
-        timestamp: 1578366393250,
-      },
-      {
-        author: { username: "user2", id: 2, avatarUrl: null },
-        text: "Show two buttons",
-        type: "text",
-        timestamp: 1578366425250,
-        buttons: [
-          {
-            type: "URL",
-            title: "Yahoo",
-            payload: "http://www.yahoo.com",
-          },
-          {
-            type: "URL",
-            title: "Example",
-            payload: "http://www.example.com",
-          },
-        ],
-      },
-      {
-        author: {
-          username: "user1",
-          id: 1,
-          avatarUrl: "https://image.flaticon.com/icons/svg/2446/2446032.svg",
-        },
-        text: "What's up?",
-        type: "text",
-        timestamp: 1578366425250,
-        hasError: true,
-      },
-    ],
-  });
-  const handleClickIcon = () => {
-    // toggle showChatbox and showIcon
-    setAttr({
-      ...attr,
-      showChatbox: !attr.showChatbox,
-      showIcon: !attr.showIcon,
-    });
-  };
-  const handleOnSendMessage = (message:any) => {
-    setAttr({
-      ...attr,
-      messages: attr.messages.concat({
-        author: {
-          username: "user1",
-          id: 1,
-          avatarUrl: "https://image.flaticon.com/icons/svg/2446/2446032.svg",
-        },
-        text: message,
-        type: "text",
-        timestamp: +new Date(),
-      }),
-    });
-  };
-
-
+ 
 
 
 
@@ -98,9 +23,9 @@ const LiveChatButton = () => {
         open={open}
         placement="leftTop"
         title={
-          <div className="p-2 shadow-lg">
+          <span>
             {/* header */}
-            <div className="flex justify-between items-center ">
+            {/* <div className="flex justify-between items-center ">
               <div className="font-bold md:text-3xl text-lg font-playfair">
                 Live Chat
               </div>
@@ -112,10 +37,10 @@ const LiveChatButton = () => {
                   <FaMinus />
                 </div>
               </Tooltip>
-            </div>
+            </div> */}
 
             {/* body */}
-            <div className="h-[60px] w-full md:my-[30px] mt-[10px] flex flex-col justify-center items-center ">
+            {/* <div className="h-[60px] w-full md:my-[30px] mt-[10px] flex flex-col justify-center items-center ">
               <Badge size="default" status="success" dot>
                 <Image
                   src={"https://i.ibb.co/YcjhGgs/IMG-20231111-142014-1.jpg"}
@@ -127,18 +52,13 @@ const LiveChatButton = () => {
               </Badge>
 
               <h3 className="mt-2 font-inter text-sm">Md Mahafujur Rahaman</h3>
-            </div>
+            </div> */}
 
             {/* Chat */}
-            <ChatBox
-              onSendMessage={handleOnSendMessage}
-              userId={1}
-              messages={attr.messages}
-              width={"300px"}
-              showTypingIndicator={true}
-              activeAuthor={{ username: "user2", id: 2, avatarUrl: null }}
-            />
-          </div>
+
+            
+           
+          </span>
         }
         trigger="click"
         body={DraggableLiveModal}
