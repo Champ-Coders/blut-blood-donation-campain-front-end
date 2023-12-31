2f17df3ebb47d05ae35df21d9a7ca0e78e145fae
Hasan-Rifat <developerhr000@gmail.com>
2023-12-31 20:30:58 +0600

    Add Button component and update user list display

--- src/app/(route)/donor-list/page.tsx
+++ src/app/(route)/donor-list/page.tsx
@@ -17,6 +17,22 @@ import DonateListSearch from "@/components/DonateList/DonateListSearch";
 import DonateList from "@/components/DonateList/DonateList";
 import Image from "next/image";
 import { people } from "@/constants/People";
+import Button from "@/components/Button/Button";
+
+interface Person {
+  address: string;
+  _id: string;
+  name: string;
+  phoneNumber: string;
+  email: string;
+  role: string;
+  bloodGroup: string;
+  dateOfBirth: string;
+  totalDonation: number;
+  totalReceived: number;
+  available: boolean;
+  __v: number;
+}
 
 const AllUsers = () => {
   const { area, bloodGroup, district, name } = useAppSelector(
@@ -156,47 +172,68 @@ const AllUsers = () => {
               onTableChange={onTableChange}
               showPagination={true}
             /> */}
-      {/* <DonateList data={users?.data?.data} />
-       */}
-      <section>
+      <DonateList data={users?.data?.data} />
+    </main>
+  );
+};
+export default AllUsers;
+
+/* 
+
+  <section>
         <div className="common">
-          <ul
-            role="list"
-            className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
-          >
-            {people.map((person) => (
+          <ul className="grid grid-cols-1 gap-6 ">
+            {users?.data?.data.map((person: Person) => (
               <li
                 key={person.email}
                 className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
               >
                 <div className="flex w-full items-center justify-between space-x-6 p-6">
-                  <div className="flex-1 truncate">
-                    <div className="flex items-center space-x-3">
+                  <div className="flex-1 flex gap-6 items-center  flex-row truncate">
+                    <div className="flex items-center space-x-3 ">
                       <h3 className="truncate text-sm font-medium text-gray-900">
-                        {person.name}
+                        Name {person.name}
                       </h3>
                       <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                         {person.role}
                       </span>
                     </div>
                     <p className="mt-1 truncate text-sm text-gray-500">
-                      {person.title}
+                      Email {person.email}
                     </p>
                   </div>
-                  <Image
+                  <div>
+                    <span className="text-sm font-medium text-gray-900">
+                      Blood Group {person.bloodGroup}
+                    </span>
+                  </div>
+
+                  <div>
+                    <p>Number {person.phoneNumber}</p>
+                  </div>
+                  <div>
+                    <p>Donation {person.totalDonation}</p>
+                  </div>
+                  <div>
+                    <p>Total Receive {person.totalReceived}</p>
+                  </div>
+                  <div>
+                    <Button type="submit" className="p-2">
+                      {person.available ? "Request" : "Unavailable"}
+                    </Button>
+                  </div>
+                <Image
                     className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
-                    src={person.imageUrl}
+                    src={person.imageUrl ? person.imageUrl : people}
                     height={40}
                     width={40}
                     alt=""
                   />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-    </main>
-  );
-};
-export default AllUsers;
+              }
+                  </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+*/
