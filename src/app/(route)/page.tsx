55867df09758d64433f7748e9f218dc7e732154e
masud90895 <masudhossainmbs129@gmail.com>
2024-01-01 18:35:25 +0600

    Refactor component imports and remove unused code

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,44 +1,43 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// import BloodOwner from "@/components/Home/BloodOwner";
-// import CallNow from "@/components/Home/CallNow";
-// import Counter from "@/components/Home/Counter";
-// import HelpThePeopleInNeed from "@/components/Home/HelpThePeopleInNeed";
-// import HomeEvent from "@/components/Home/HomeEvent";
-// import News from "@/components/Home/News";
-// import PopularCampaigns from "@/components/Home/PopularCampaigns";
-// import Services from "@/components/Home/Services";
-// import TeamMember from "@/components/Home/TeamMember";
-
-// import FaqQuestion from "@/components/Home/FaqQuestion";
-// import ListOfHonorableBloodDonors from "@/components/Home/ListOfHonorableBloodDonors";
-// import FeedBackForum from "@/components/FeedBackForum/FeedBackForum";
-
 import VideoBanner from "@/components/Home/VideoBanner.jsx";
 import BloodTypeDonors from "@/components/Home/BloodTypeDonors";
 import { FloatButton } from "antd";
 import { VerticalAlignTopOutlined } from "@ant-design/icons";
 import Testimonials from "@/components/Home/Testimonials";
+import ListOfHonorableBloodDonors from "@/components/Home/ListOfHonorableBloodDonors";
+import HomeEvent from "@/components/Home/HomeEvent";
+import HelpThePeopleInNeed from "@/components/Home/HelpThePeopleInNeed";
+import Counter from "@/components/Home/Counter";
+import Services from "@/components/Home/Services";
+import CallNow from "@/components/Home/CallNow";
+import PopularCampaigns from "@/components/Home/PopularCampaigns";
+import BloodOwner from "@/components/Home/BloodOwner";
+import TeamMember from "@/components/Home/TeamMember";
+import News from "@/components/Home/News";
+import FaqQuestion from "@/components/Home/FaqQuestion";
+import FeedBackForum from "@/components/FeedBackForum/FeedBackForum";
 export default function Home() {
   return (
     <>
       <VideoBanner />
       <BloodTypeDonors />
-      {/*
       <ListOfHonorableBloodDonors />
       <HomeEvent />
       <HelpThePeopleInNeed />
       <Counter />
       <Services />
       <CallNow />
-      <PopularCampaigns /> */}
+
+      <PopularCampaigns />
       <Testimonials />
-      {/* <BloodOwner />
+      <BloodOwner />
       <TeamMember />
       <News />
-      <FaqQuestion />
-      <FeedBackForum /> */}
+      {/* <FaqQuestion /> */}
+      <FeedBackForum />
+
       {/* floating icon */}
       <FloatButton.BackTop
         shape="square"
