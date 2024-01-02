0b0ac59d94dcedb897ec48c1eaf9ab9fff7063ea
sarwar-asik <sarwarasik@gmail.com>
2024-01-02 11:44:26 +0600

    chat_layout

--- src/app/(dashboard)/admin/chat/page.tsx
+++ src/app/(dashboard)/admin/chat/page.tsx
@@ -1,5 +1,12 @@
+import ChatContainer from "@/components/chat/ChatContainer";
+import ChatSidebar from "@/components/chat/ChatSidebar";
 import React from "react";
 
 export default function ChatAdminnPage() {
-  return <div>ChatAdminnPage</div>;
+  return (
+    <div className="flex h-screen overflow-hidden relative">
+      <ChatSidebar />
+      <ChatContainer />
+    </div>
+  );
 }
