71d6e3f0e6ebbc3107b3798ba0d015cb86d6803f
masud90895 <masudhossainmbs129@gmail.com>
2024-01-03 15:30:13 +0600

    Refactor DonateList component to include profile image, email, phone number, blood group, and availability. Add request button based on availability.

--- src/components/DonateList/DonateList.tsx
+++ src/components/DonateList/DonateList.tsx
@@ -2,11 +2,14 @@ import { IUser } from "@/interfaces/common";
 import React from "react";
 import NoImage from "../../../public/assets/icon/userIcon.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const DonateList = ({ data }: { data: IUser }) => {
   return (
-    <div className="bg-white shadow-md border p-3 rounded-lg flex justify-between items-center">
-      <div className="flex items-center">
+    <div className="bg-white shadow-md border p-3 rounded-lg  text-[14px] grid lg:grid-cols-4 justify-between items-center">
+      {/* profile */}
+
+      <div className="flex items-center flex-col md:flex-row ">
         <Image
           className="w-10 h-10 rounded-full mr-4"
           src={data?.imgUrl ?? NoImage}
@@ -14,15 +17,51 @@ const DonateList = ({ data }: { data: IUser }) => {
           width={40}
           height={40}
         />
-        <div className="text-sm">
+        <div className="text-sm flex flex-col gap-1">
           <p className="text-gray-900 leading-none">{data?.name}</p>
-          <p className="text-gray-600">{data?.email}</p>
+          <p className="text-gray-600 text-[12px] ">{data?.email}</p>
+          {/* Phone */}
+          <p className="text-gray-900 leading-none text-[12px]">
+            {data?.phoneNumber}
+          </p>
         </div>
       </div>
 
       {/* Blood Group */}
 
-      <p className="text-gray-900 leading-none">{data?.bloodGroup}</p>
+      <p className="text-gray-900 leading-none text-center">
+        {data?.bloodGroup}
+      </p>
+
+      {/* Available */}
+      <p className="text-gray-900 leading-none">
+        {data?.available === true ? (
+          <span className="text-green-900 text-[12px] bg-green-200 rounded-full p-2">
+            Available
+          </span>
+        ) : (
+          <span className="text-red-500 text-[12px] bg-primary/30 rounded-full p-2 ">
+            Unavailable
+          </span>
+        )}
+      </p>
+
+      {/* request for blood */}
+      {
+        // @ts-ignore
+        data?.available === true ? (
+          <Link
+            href={""}
+            className="bg-primary text-white px-4 py-2 rounded-md text-center"
+          >
+            Request
+          </Link>
+        ) : (
+          <button className="bg-primary/30 text-white px-4 py-2 rounded-md text-center">
+            Request
+          </button>
+        )
+      }
     </div>
   );
 };
