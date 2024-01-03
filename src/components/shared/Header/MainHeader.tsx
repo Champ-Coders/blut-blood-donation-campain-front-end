8a03e8657f515748fb6362ddb0513a0a66ddd942
masud90895 <masudhossainmbs129@gmail.com>
2024-01-03 21:19:36 +0600

    Update MainHeader and config files

--- src/components/shared/Header/MainHeader.tsx
+++ src/components/shared/Header/MainHeader.tsx
@@ -112,7 +112,7 @@ const MainHeader = () => {
                     <Menu.Item>
                       {({ active }) => (
                         <Link
-                          href="/dashboard"
+                          href="/profile"
                           className={classNames(
                             active ? "bg-gray-100" : "",
                             "block px-4 py-2 text-sm text-gray-700"
