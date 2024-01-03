8476d143f43ec4e0ec90ff770d9c69043853ed0d
sarwar-asik <sarwarasik@gmail.com>
2024-01-03 19:06:55 +0600

    removed_extra

--- src/app/(dashboard)/(chat)/chat/page.tsx
+++ src/app/(dashboard)/(chat)/chat/page.tsx
@@ -12,8 +12,8 @@ export default function ChatAdminnPage() {
     //   <ChatContainer />
     // </div>
     <div className="">
-      <h3>main chat page</h3>
-      <ChatContainer  senderId="11111"/>
+      {/* <h3>main chat page</h3> */}
+      <ChatContainer senderId="11111" />
     </div>
   );
 }
