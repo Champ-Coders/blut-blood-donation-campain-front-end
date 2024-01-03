70c771c64fe15b807f72de0b2932a845770e91b4
masud90895 <masudhossainmbs129@gmail.com>
2024-01-03 19:54:07 +0600

    Refactor blogCommentApi.ts to remove unused imports and update createBlogComment endpoint

--- src/redux/Api/blogCommentApi/blogCommentApi.ts
+++ src/redux/Api/blogCommentApi/blogCommentApi.ts
@@ -1,7 +1,4 @@
-import {
-  getFromLocalStorage,
-  setToLocalStorage,
-} from "../../../utils/local-storage";
+import { getFromLocalStorage } from "../../../utils/local-storage";
 import { api } from "../api";
 import { tagTypes } from "../tagsType";
 
@@ -9,40 +6,17 @@ const BLOG_COMMENT_URL = "/blog-comment";
 
 export const donationAPi = api.injectEndpoints({
   endpoints: (build) => ({
-    // get all blog comments
-    getAllComment: build.query({
-      query: () => ({
+    // create blog comment
+    createBlogComment: build.mutation({
+      query: (data: { userId: string; blogId: string; comments: string }) => ({
         url: `${BLOG_COMMENT_URL}`,
-        method: "GET",
+        method: "POST",
+        body: data,
         headers: {
           Authorization: `${getFromLocalStorage("user")}`,
         },
       }),
-      providesTags: [tagTypes.blogComment],
-    }),
-
-    // get single blog comment
-    getSingleBlogComment: build.query({
-      query: (id) => ({
-        url: `${BLOG_COMMENT_URL}/${id}`,
-        method: "GET",
-        headers: {
-          Authorization: `${getFromLocalStorage("user")}`,
-        },
-      }),
-    }),
-
-    // replay blog comment
-    replayBlogComment: build.mutation({
-      query: (data) => ({
-        url: `${BLOG_COMMENT_URL}/${data.id}`,
-        method: "PATCH",
-        body: data.data,
-        headers: {
-          Authorization: `${getFromLocalStorage("user")}`,
-        },
-      }),
-      invalidatesTags: [tagTypes.blogComment],
+      invalidatesTags: [tagTypes.blogComment, tagTypes.blog],
     }),
 
     // delete blog comment
@@ -54,28 +28,10 @@ export const donationAPi = api.injectEndpoints({
           Authorization: `${getFromLocalStorage("user")}`,
         },
       }),
-      invalidatesTags: [tagTypes.blogComment],
-    }),
-
-    // create blog comment
-    createBlogComment: build.mutation({
-      query: (data) => ({
-        url: `${BLOG_COMMENT_URL}/create-blog`,
-        method: "POST",
-        body: data,
-        headers: {
-          Authorization: `${getFromLocalStorage("user")}`,
-        },
-      }),
-      invalidatesTags: [tagTypes.blogComment],
+      invalidatesTags: [tagTypes.blogComment, tagTypes.blog],
     }),
   }),
 });
 
-export const {
-  useGetAllCommentQuery,
-  useGetSingleBlogCommentQuery,
-  useReplayBlogCommentMutation,
-  useDeleteBlogCommentMutation,
-  useCreateBlogCommentMutation,
-} = donationAPi;
+export const { useDeleteBlogCommentMutation, useCreateBlogCommentMutation } =
+  donationAPi;
