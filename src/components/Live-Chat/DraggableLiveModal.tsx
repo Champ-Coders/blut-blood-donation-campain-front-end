ca95b9cd43c8a73cfd0f64f78436cdfcef9cf272
masud90895 <masudhossainmbs129@gmail.com>
2024-01-04 22:39:13 +0600

    Update API base URL and Live Chat button design

--- src/components/Live-Chat/DraggableLiveModal.tsx
+++ src/components/Live-Chat/DraggableLiveModal.tsx
@@ -35,6 +35,7 @@ const DraggableLiveModal = () => {
 
   const [refreshChat] = useRefreshChatMutation();
   // console.log("🚀 ~MessageData:", MessageData);
+  const [messages, setMessages] = useState<any>("");
 
   const [chatMessages, setChatMessages] = React.useState<any>(
     MessageData?.data || []
@@ -44,10 +45,12 @@ const DraggableLiveModal = () => {
 
     if (!userInfo) {
       return message.error("Please login first");
+    } else if (!messages) {
+      return message.error("Please write something");
     } else {
       const newMessage = {
         // id: chatMessages.length + 1,
-        message: data.message,
+        message: messages,
         time: new Date().toLocaleTimeString(),
         img: userInfo?.imgUrl || "",
         status: "online",
@@ -61,6 +64,7 @@ const DraggableLiveModal = () => {
         // console.log("uuuuu", data);
         refreshChat(data);
         reset();
+        setMessages("");
       });
     }
   };
@@ -85,102 +89,13 @@ const DraggableLiveModal = () => {
 
   // console.log('messageData',chatMessages)
   return (
-    <div>
+    <div className="lg:w-[500px] ">
       <div className="flex-1 p-2 justify-between flex flex-col h-[500px]">
-        {/* header */}
-        <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-          <div className="relative flex items-center space-x-4">
-            <div className="relative">
-              <span className="absolute text-green-500 right-0 bottom-0">
-                <svg width="20" height="20">
-                  <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
-                </svg>
-              </span>
-              <Image
-                src={
-                  "https://i.ibb.co/7xDVRjP/3d-illustration-businessman-with-headphone-blurred-background-business-concept.jpg"
-                }
-                alt="Live Chat"
-                width={4000}
-                height={4000}
-                className="w-[50px] sm:w-10 h-[50px] rounded-full"
-              />
-            </div>
-            <div className="flex flex-col leading-tight">
-              <div className="text-2xl mt-1 flex items-center">
-                <span className="text-gray-700 mr-3 text-[18px]">Mr. Joe</span>
-              </div>
-              <span className="text-[12px] text-gray-600">
-                Coperative Officer
-              </span>
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </button>
-            <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-            </button>
-            <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </div>
         {/* message */}
         <div
           id="messages"
           className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch "
         >
-          {isLoading || (chatMessages?.length < 1 && <ChatSkelleton />)}
           {
             // chatMessages
             //   ?.sort((a: any, b: any) => a.updatedAt - b.updatedAt)
@@ -236,7 +151,7 @@ const DraggableLiveModal = () => {
         {/* button */}
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="border-t-2 border-gray-200 bg-red-300 px-4 pt-4 mb-2 sm:mb-0"
+          className="border-t-2 border-gray-200 bg-red-300 p-4 mb-2 sm:mb-0"
         >
           <div className="relative flex">
             <span className="absolute inset-y-0 flex items-center">
@@ -262,9 +177,9 @@ const DraggableLiveModal = () => {
             </span>
             {/* chat input */}
             <InputEmoji
-              onChange={(text) => setValue("message", text)}
-              cleanOnEnter
-              value=""
+              onChange={(text) => setMessages(text)}
+              // cleanOnEnter
+              value={messages}
             />
 
             <button
