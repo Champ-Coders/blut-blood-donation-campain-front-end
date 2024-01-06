9377864c913a5a6625baf48754042094617d6b66
masud90895 <masudhossainmbs129@gmail.com>
2024-01-06 14:05:42 +0600

    Fix login auto-fill buttons and update header styling

--- src/components/shared/Header/MainHeader.tsx
+++ src/components/shared/Header/MainHeader.tsx
@@ -151,7 +151,7 @@ const MainHeader = () => {
                           onClick={SignOutHandler}
                           className={classNames(
                             active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700  hover:bg-primary  hover:text-white  mb-0"
+                            "block px-4 py-2 text-sm text-gray-700  hover:bg-primary  hover:text-white  mb-0 w-full text-start"
                           )}
                         >
                           Sign out
